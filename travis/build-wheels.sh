2ee2e78859619511ea443ae86b35b88ece9a9981
Dan Blanchard <dan.blanchard@gmail.com>
2020-03-17 14:45:03 -0400

    Remove another missing library

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -18,9 +18,8 @@ function repair_wheel {
 # Install packages required for wheel building
 yum install -y zlib-devel bzip2-devel ncurses-devel sqlite-devel \
 readline-devel tk-devel gdbm-devel db4-devel libpcap-devel\
-xz-devel gpg atlas-devel libev-devel libev snappy-devel
-openjpeg-devel freetype-devel libpng-devel \
-libffi-devel python-lxml postgresql95-libs \
+xz-devel gpg atlas-devel libev-devel libev snappy-devel freetype-devel \
+libpng-devel libffi-devel python-lxml postgresql95-libs \
 postgresql95-devel lapack-devel python \
 python-devel python-setuptools pcre pcre-devel \
 pandoc
